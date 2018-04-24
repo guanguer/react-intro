4bdbafb080fd152578d1108153d9b2953cc5d1c0
Juan Fernando Taborda Sanchez <juan.taborda@globant.com>
2018-04-24 00:44:20 -0500

    feat: search using state

--- src/search.jsx
+++ src/search.jsx
@@ -1,12 +1,54 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import data from '../data.json';
 import ShowCard from './show-card';
 
-const Search = () => (
-    <div className="search">
-        {data.shows.map(show => <ShowCard key={show.imdbID} show={show} />)}
-    </div>
-);
+const ShowList = styled.div`
+    display: flex;
+    flex-flow: row wrap;
+    justify-content: space-between;
+    align-content: stretch;
+`;
+
+class Search extends React.Component {
+    state = {
+        searchTerm: ''
+    };
+
+    handleSearchTermChange = event => {
+        this.setState({ searchTerm: event.target.value });
+    };
+
+    isSearchTermInTitleOrDescription = show =>
+        `${show.title} ${show.description}`
+            .toUpperCase()
+            .indexOf(this.state.searchTerm.toUpperCase()) >= 0;
+
+    render() {
+        return (
+            <div className="search">
+                <header>
+                    <h1>svideo</h1>
+                    <input
+                        type="text"
+                        value={this.state.searchTerm}
+                        onChange={this.handleSearchTermChange}
+                        placeholder="Search"
+                    />
+                </header>
+                <ShowList>
+                    {data.shows
+                        .filter(show =>
+                            this.isSearchTermInTitleOrDescription(show)
+                        )
+                        .map(show => (
+                            <ShowCard key={show.imdbID} show={show} />
+                        ))}
+                </ShowList>
+            </div>
+        );
+    }
+}
 
 export default Search;
--- src/show-card.jsx
+++ src/show-card.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
 import { shape, string } from 'prop-types';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+    flex-basis: 100%;
+    border: 2px solid #333;
+    border-radius: 4px;
+    margin-bottom: 25px;
+    overflow: hidden;
+
+    @media (min-width: 790px) {
+        flex-basis: 44%;
+    }
+
+    @media (min-width: 1100px) {
+        flex-basis: 32%;
+    }
+`;
+
+const Image = styled.img`
+    width: 46%;
+    height: 100%;
+    float: left;
+    margin-right: 5px;
+`;
 
 const ShowCard = props => (
-    <div className="show-card">
-        <img
+    <Wrapper>
+        <Image
             src={`img/posters/${props.show.poster}`}
             alt={`${props.show.title} Show Poster`}
         />
@@ -12,7 +36,7 @@ const ShowCard = props => (
             <h4>({props.show.year})</h4>
             <p>{props.show.description}</p>
         </div>
-    </div>
+    </Wrapper>
 );
 
 ShowCard.propTypes = {
